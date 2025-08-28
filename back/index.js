e7d27c3ea6b09da3263491686f753bef5a8d0b4c
vorja <pborja564@gmail.com>
2025-08-28 10:21:19 -0500

    arreglo de errores

--- back/index.js
+++ back/index.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-require('dotenv').config()
+import 'dotenv/config';
 
 
 app.use(express.json());
